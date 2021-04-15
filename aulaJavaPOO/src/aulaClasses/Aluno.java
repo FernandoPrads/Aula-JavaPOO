b473ab62261edf3d4591641147a90e3796e4843d
ferna <ferna@DESKTOP-CV3I14L>
2021-04-15 19:49:21 -0300

    primeiras aulas de JavaPOO

--- aulaJavaPOO/src/aulaClasses/Aluno.java
+++ aulaJavaPOO/src/aulaClasses/Aluno.java
@@ -1,5 +1,29 @@
 package aulaClasses;
 
 public class Aluno {
+	
+	/*Esses são os atributos do aluno*/
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	public Aluno() {
+		
+	}
+	
+	public Aluno (String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+	
+	}
+	
 
-}
--- aulaJavaPOO/src/aulaClasses/primeiraClasee.java
+++ aulaJavaPOO/src/aulaClasses/primeiraClasee.java
@@ -1,5 +1,21 @@
 package aulaClasses;
 
+import aulaClasses.Aluno;
+
 public class primeiraClasee {
+
+	
+	
+	public static void main(String[]args) {
+		
+		/*new Aluno() é uma instancia (criação de Objetos)*/
+		/*aluno1 é uma referencia para o objeto aluno*/
+		
+		Aluno aluno1 = new Aluno();
+		Aluno aluno2 = new Aluno();
+		Aluno aluno3 = new Aluno();
+		Aluno aluno4 = new Aluno("Maria");
+		
+	}
 
 }
